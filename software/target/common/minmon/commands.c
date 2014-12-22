2945a9bc3a137d184eb7571224ace60dc11f813e
Shane Gough <shane@thegaragelab.com>
2014-12-22 15:30:32 +1000

    Started implementing basic functionality of MinMon as well as splitting it into multiple implementation files.

---
+++ software/target/common/minmon/commands.c
@@ -0,0 +1,57 @@
+/*--------------------------------------------------------------------------*
+* Command definitions and lookup
+*---------------------------------------------------------------------------*
+* 22-Dec-2014 ShaneG
+*
+* Defines the text version of the commands (and aliases) and provides a
+* method to identify the command.
+*--------------------------------------------------------------------------*/
+#include <stdint.h>
+#include <stdbool.h>
+#include "minmon.h"
+
+/** Command text to ID mapping
+ */
+typedef struct _CMDINFO {
+  const char      *m_cszCommand; //!< The text for the command or alias
+  const MINMON_CMD m_command;    //!< The command ID
+  } CMDINFO;
+
+/** Command definitions
+ *
+ * This structure maps command text to a command ID. It is terminated by an
+ * entry with the command ID set to CMD_INVALID.
+ */
+static CMDINFO s_commands[] = {
+  { "A",     CMD_ADDRESS },
+  { "ADDR",  CMD_ADDRESS },
+  { "R",     CMD_READ    },
+  { "READ",  CMD_READ    },
+  { "W",     CMD_WRITE   },
+  { "WRITE", CMD_WRITE   },
+  { "G",     CMD_GOTO    },
+  { "GOTO",  CMD_GOTO    },
+  { "",      CMD_INVALID }
+  };
+
+/** Get the command ID from a string
+ *
+ * Convert a string into a command ID.
+ *
+ * @param cszCmd pointer to the string containing the command name.
+ * @param length the length of the string in characters
+ *
+ * @return the command ID or CMD_INVALID if the string does not represent a
+ *         valid command.
+ */
+MINMON_CMD cmdLookup(const char *cszCmd, uint8_t length) {
+  uint8_t index = 0;
+  showHex(length, 2);
+  while(s_commands[index].m_command!=CMD_INVALID) {
+    if(strncmp(cszCmd, s_commands[index].m_cszCommand, length))
+      return s_commands[index].m_command;
+    index++;
+    }
+  return CMD_INVALID;
+  }
+
--- software/target/common/minmon/minmon.c
+++ software/target/common/minmon/minmon.c
@@ -7,6 +7,7 @@
 *---------------------------------------------------------------------------*/
 #include <stdint.h>
 #include <stdbool.h>
+#include "minmon.h"
 #include "tgl6502.h"
 
 //! Banner to display
@@ -33,24 +34,11 @@ char g_line[LINE_LENGTH];
 //! Line length
 uint8_t g_length;
 
-/** Display a hex value
- */
-static void showHex(uint16_t value, int digits) {
-  uint8_t current, offset = (digits - 1) * 4;
-  uint16_t mask = 0x000f << offset;
-  while(mask) {
-    current = (value & mask) >> offset;
-    if(current>=10)
-      putch('A' + current - 10);
-    else
-      putch('0' + current);
-    // Adjust mask and offset
-    mask = mask >> 4;
-    offset -= 4;
-    }
-  }
-
 /** Read a line of input
+ *
+ * Reads a line of input into the global 'g_line' variable and sets the line
+ * length in 'g_length'. This function performs basic line editing (backspace
+ * and character echo) and limits the allowed input characters.
  */
 static void readLine() {
   uint8_t ch;
@@ -92,10 +80,71 @@ static void readLine() {
     }
   }
 
+typedef enum {
+  TOKEN_CMD,   //!< A command string
+  TOKEN_BYTE,  //!< A two digit byte
+  TOKEN_WORD,  //!< A four digit word
+  TOKEN_EOL,   //!< End of input line
+  TOKEN_ERROR, //!< Unrecognised token
+  } TOKEN;
+
+static TOKEN getToken(uint8_t *pIndex, uint16_t *pValue) {
+  uint8_t length;
+  const char *cszStart;
+  // Skip spaces
+  while((g_line[*pIndex]==' ')&&(*pIndex<g_length))
+    *pIndex++;
+  // Check for EOL
+  if(*pIndex>=g_length)
+    return TOKEN_EOL;
+  // Check for hex constants
+  if(g_line[*pIndex++]=='$') {
+    cszStart = &g_line[*pIndex];
+    length = 0;
+    while((*pIndex<g_length)&&ishex(g_line[*pIndex])) {
+      *pIndex = *pIndex + 1;
+      length++;
+      }
+    *pValue = hexToWord(cszStart, length);
+    if(length==2)
+      return TOKEN_BYTE;
+    if(length==4)
+      return TOKEN_WORD;
+    return TOKEN_ERROR;
+    }
+  // Check for commands
+  if((g_line[*pIndex]>='A')&&(g_line[*pIndex]<='Z')) {
+    cszStart = &g_line[*pIndex];
+    length = 0;
+    while((*pIndex<g_length)&&((g_line[*pIndex]>='A')&&(g_line[*pIndex]<='Z'))) {
+      *pIndex = *pIndex + 1;
+      length++;
+      puts("CMD length = ");
+      showHex(length, 2);
+      puts(" g_length = ");
+      showHex(g_length, 2);
+      puts(" *pIndex = ");
+      showHex(*pIndex, 2);
+      puts("\r\n");
+      }
+    *pValue = cmdLookup(cszStart, length);
+    return (*pValue==CMD_INVALID)?TOKEN_ERROR:TOKEN_CMD;
+    }
+  // Invalid input
+  return TOKEN_ERROR;
+  }
+
 /** Program entry point
  *
  */
 void main() {
+  bool incaddr;
+  uint8_t index;
+  MINMON_CMD cmd;
+  uint16_t addr;
+  uint16_t value;
+  TOKEN token;
+  // Do some identification
   puts(BANNER "\n");
   // TODO: Copy ourselves to RAM and swap pages
   // Go into main processing loop
@@ -106,7 +155,65 @@ void main() {
     putch('>');
     // Read the next line
     readLine();
-    // TODO: Read commands and process
+    index = 0;
+    incaddr = false;
+    // Get the first token
+    token = getToken(&index, &value);
+    // Check for command and default to 'READ'
+    if(token==TOKEN_CMD) {
+      cmd = value;
+      token = getToken(&index, &value);
+      }
+    else
+      cmd = CMD_READ;
+    // Check for an address and default to current address
+    if(token==TOKEN_WORD) {
+      addr = value;
+      token = getToken(&index, &value);
+      }
+    else {
+      addr = g_addr;
+      incaddr = true;
+      }
+    // Check for errors
+    if(token==TOKEN_ERROR) {
+      puts("ERROR: Unrecognised command.\r\n");
+      continue;
+      }
+    // Process command
+    switch(cmd) {
+      case CMD_READ:
+        // Read 16 bytes and display them
+        putch('$');
+        showHex(addr, 4);
+        putch(':');
+        for(index=0;index<16;index++,addr++) {
+          puts(" $");
+          showHex(*((uint8_t *)addr), 2);
+          }
+        puts("\r\n");
+        break;
+      case CMD_ADDRESS:
+        incaddr = true;
+        break;
+      case CMD_WRITE:
+        // Write bytes to memory
+        while(token==TOKEN_BYTE) {
+          *((uint8_t *)addr) = (uint8_t)(value & 0xFF);
+          addr++;
+          token = getToken(&index, &value);
+          }
+        break;
+      default:
+        puts("ERROR: Command not implemented.\r\n");
+        continue;
+      }
+    // Be neat
+    if(token!=TOKEN_EOL)
+      puts("WARNING: Additional content at end of line was ignored.\r\n");
+    // Update current address if required
+    if(incaddr)
+      g_addr = addr;
     }
   }
 
